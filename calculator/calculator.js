816b83d1badddfedce1991fdea6aeb4c52f5e82e
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-04-26 17:10:36 +0300

    Solved calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,33 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((current, next) => current + next, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((current, next) => current * next, 1);
 }
 
-function power() {
-	
+function power(a, b) {
+	return a**b;
 }
 
-function factorial() {
-	
+function factorial(number) {
+	if (number === '0') return 1;
+
+	let result = 1;
+	let i = 1;
+	while (i <= number) {
+		result *= i;
+		i++;
+	}
+	return result;
 }
 
 module.exports = {
