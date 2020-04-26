47edd2c8434ede96cc3d40be9e866cd3543edf36
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-04-26 17:53:29 +0300

    Finished findTheOldest test suite

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-let findTheOldest = function() {
+let findTheOldest = function(persons) {
+    return persons.reduce((curentPerson, nextPerson) => {
+        const date = new Date();
+        const curentYear = date.getFullYear();
 
+        if (!curentPerson.yearOfDeath) curentPerson.yearOfDeath = curentYear;
+        if (!nextPerson.yearOfDeath) nextPerson.yearOfDeath = curentYear;
+
+        return (curentPerson.yearOfDeath - curentPerson.yearOfBirth > nextPerson.yearOfDeath - nextPerson.yearOfBirth)
+                ? curentPerson
+                : nextPerson;
+    }, [])
 }
 
 module.exports = findTheOldest
