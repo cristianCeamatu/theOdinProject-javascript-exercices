63f809379ce5708cb0fad0068070f6334a39983d
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-04-26 14:03:05 +0300

    Finished hello world and caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,35 @@
-const caesar = function() {
+const caesar = function(string, shifts) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let caesarArray = [];
+    if (Math.abs(shifts) > alphabet.length) shifts %= alphabet.length;
 
+    for(let i = 0; i < string.length; i++) {
+        const char = string[i];
+        let charIndex = alphabet.indexOf(char.toLowerCase());
+
+        if (charIndex < 0) {
+            caesarArray.push(char);
+            continue;
+        } else if (charIndex + shifts > alphabet.length - 1) {
+            charIndex = charIndex - alphabet.length;
+        } else if (shifts < 0) {
+            if ((charIndex - Math.abs(shifts)) < 0) {
+                charIndex = alphabet.length + charIndex;
+            }
+            let caesarChar = isUpperCase(char) ? alphabet[charIndex - Math.abs(shifts)].toUpperCase() : alphabet[charIndex - Math.abs(shifts)];
+            
+            caesarArray.push(caesarChar);
+            continue;
+        }
+
+        let caesarChar = isUpperCase(char) ? alphabet[charIndex + shifts].toUpperCase() : alphabet[charIndex + shifts];
+        caesarArray.push(caesarChar);
+    }
+    return caesarArray.join('');
+}
+
+const isUpperCase = function(char) {
+    return (char.toUpperCase() === char) ? true : false;
 }
 
 module.exports = caesar
